85af77515a9212fb5fc504d363942e33729d9f55
David Semedo <df.semedo@campus.fct.unl.pt>
2018-06-07 14:36:38 +0100

    Improve evaluation script output. Added some comments Added example run.

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -2,20 +2,44 @@ import numpy
 import pandas
 import json
 import sys
+from termcolor import colored
+
+# Metric Parameters
+# These parameters should not be changed. Check Guidelines document for a description of each parameter.
+alpha = 0.1
+beta  = 0.6
+
+event_names_abbreviations ={
+	"Edinburgh Festival 2016": "edfest_2016",
+	"Tour de France 2016": "tourfrance_2016",
+	"Edinburgh Festival 2017": "edfest_2017",
+	"Tour de France 2017": "tourfrance_2017"
+}
 
 n_args = len(sys.argv)
 if n_args != 3 and n_args != 5:
     print("usage:")
-    print("python evaluate.py stories_json evaluation_file [relevance_groundtruth] [transitions_groundtruth]")
+    print("python evaluate.py <event_stories.json> <result.run> <event_relevance_groundtruth.csv>  <event_transitions_groundtruth.csv>")
     exit()
 
+# Load Stories and Segments
 story_path = sys.argv[1]
-stories = json.load(open(story_path))["stories"]
+stories_json = json.load(open(story_path))
+stories = stories_json["stories"]
+event = event_names_abbreviations[stories_json["event_name"]]
+
+# Load run file
 to_evaluate_path = sys.argv[2]
 to_evaluate = pandas.read_csv(to_evaluate_path, delim_whitespace=True, dtype={"run_id": numpy.str, "query_id": numpy.str, "dummy": numpy.str, "doc_id": numpy.str})
+
+# Load Groundtruth if specified
 if n_args == 5:
     relevance_gt_path = sys.argv[3]
     transitions_gt_path = sys.argv[4]
+
+    # Make sure stories and groundtruth files match
+    assert((event in relevance_gt_path) and (event in transitions_gt_path)), "Event should be the same for story topics and groundtruth."
+
     relevance_gt = pandas.read_csv(relevance_gt_path, dtype={"query_id": numpy.str, "doc_id": numpy.str, "rel": numpy.int32})
     transitions_gt = pandas.read_csv(transitions_gt_path, dtype={"query_id": numpy.str, "doc_id1": numpy.str, "doc_id2": numpy.str, "trans": numpy.int32})
 
@@ -27,6 +51,7 @@ def main():
         evaluate()
 
 
+# Check submission file
 def test_format():
     for story in stories:
         story_id = story["story_id"]
@@ -38,19 +63,20 @@ def test_format():
             query_id = "{}.{}".format(story_id, segment["segment_id"])
             n_tries = numpy.count_nonzero(to_evaluate["query_id"] == query_id)
             if n_tries == 0:
-                print("Missing segment {}".format(segment_id))
+                print("Missing segment {} of {}".format(segment_id, len(segments)))
             elif n_tries > 1:
-                print("More then one attempt for segment {}".format(segment_id))
+                print("More than one attempt for segment {}".format(segment_id))
             if story_ok:
                 story_ok = n_tries == 1
         if story_ok:
-            print("Ok")
+            print(colored('Ok', 'green'))
+        else:
+            print(colored('Error', 'red'))
 
         print()
 
-
+# Evaluate submission
 def evaluate():
-    stories = json.load(open(story_path))["stories"]
 
     scores = []
     relevances = []
@@ -79,10 +105,10 @@ def evaluate():
         segment_attempts = sorted(segment_attempts, key=lambda x: x[0])
         if len(segment_attempts) > 0:
             relevant_list, transition_list, score = calc_story(story_id, segment_attempts)
-            print("story: ", story_id)
-            print("relevance:", " ".join(map(str, relevant_list)))
-            print("transitions:", " ".join(map(str, transition_list)))
-            print("score:", round(score, 3))
+            print("Story: ", story_id)
+            print("\tRelevance:", " ".join(map(str, relevant_list)))
+            print("\tTransitions:", " ".join(map(str, transition_list)))
+            print("\tQuality Score:", round(score, 4))
             print()
         else:
             score = 0
@@ -96,9 +122,9 @@ def evaluate():
     avg_scores = numpy.array(scores).mean()
     avg_relevances = numpy.array(relevances).mean()
     avg_transitions = numpy.array(transitions).mean()
-    print("Average score: ", round(avg_scores, 3))
-    print("Relevance precision: ", round(avg_relevances, 3))
-    print("Transitions precision: ", round(avg_transitions, 3))
+    print("Average Quality Score: ", round(avg_scores, 4))
+    print("Relevance Precision: ", round(avg_relevances, 4))
+    print("Transitions Quality: ", round(avg_transitions, 4))
 
 
 def calc_story(story_id, segment_attempts):
@@ -131,12 +157,12 @@ def calc_story(story_id, segment_attempts):
     return relevant_list, transition_list, score
 
 
-def quality(relevance, transitions, a=0.1, b=0.6):
+def quality(relevance, transitions, a=alpha, b=beta):
     pairwise_quality = 0
     for i in range(1, len(relevance)):
         pairwise_quality += b * (relevance[i] + relevance[i - 1]) + (1 - b) * (relevance[i] * relevance[i - 1] + transitions[i - 1])
 
-    return a * relevance[0] + (1 - a) / (2 * (len(relevance) - 1)) * pairwise_quality
+    return a * relevance[0] + (1 - a) / float((2 * (len(relevance) - 1))) * pairwise_quality
 
 
 if __name__ == '__main__':
